56b6a1558f86c9cbe56a0f043c1961fb5f889eab
Marcel Boller <marcel-boller@web.de>
2025-03-26 16:15:22 +0100

    remove anything

--- src/components/Table/StyledTableRow.tsx
+++
@@ -1,9 +0,0 @@
-import { styled, TableRow } from '@mui/material';
-
-const StyledTableRow = styled(TableRow)({
-  '&:nth-of-type(even)': {
-    backgroundColor: '#f9f9f9'
-  }
-});
-
-export default StyledTableRow;
